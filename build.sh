b76071664f1ee001ed68afa2c133685ed492b9ca
Mode <mode89@mail.ru>
2017-02-21 18:56:49 +0800

    configure, build and install libcxx

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 WORK_DIR=$(pwd)
 
+LINARO_DIR=/opt/linaro/gcc-linaro-arm-linux-gnueabi-2012.04-20120426_linux
+LINARO_VER=4.7.1
+
 TARGET_TRIPLE=arm-linux-gnueabi
 
 LLVM_REPO=http://llvm.org/git/llvm.git
@@ -96,6 +99,61 @@ function clean_libcxx()
     if [ -d $LIBCXX_BUILD_DIR ]; then
         rm -r $LIBCXX_BUILD_DIR
     fi
+}
+
+function config_libcxx()
+{
+    echo Configuring libcxx ...
+    if [ ! -d $LIBCXX_BUILD_DIR ]; then
+        mkdir $LIBCXX_BUILD_DIR
+    fi
+    cd $LIBCXX_BUILD_DIR
+
+    LINARO=$LINARO_DIR
+    TRIPLE=$TARGET_TRIPLE
+    VER=$LINARO_VER
+
+    SYSROOT_FLAG="--sysroot=$LINARO/$TRIPLE/libc"
+    ARCH_FLAGS="-march=armv7-a -mcpu=cortex-a9 -mfloat-abi=soft"
+
+    C_FLAGS="$SYSROOT_FLAG $ARCH_FLAGS "
+    CXX_FLAGS="$SYSROOT_FLAG $ARCH_FLAGS "
+    CXX_FLAGS+="-I $LINARO/$TRIPLE/include/c++/$VER/ "
+    CXX_FLAGS+="-I $LINARO/$TRIPLE/include/c++/$VER/$TRIPLE/ "
+
+    LINKER_FLAGS="-fuse-ld=$LINARO/bin/$TRIPLE-ld "
+    LINKER_FLAGS+="-B $LINARO/lib/gcc/$TRIPLE/$VER/ "
+    LINKER_FLAGS+="-L $LINARO/lib/gcc/$TRIPLE/$VER/ "
+    LINKER_FLAGS+="-L $LINARO/$TRIPLE/lib/ "
+
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DCMAKE_C_COMPILER=$INSTALL_DIR/bin/clang \
+        -DCMAKE_C_FLAGS="$C_FLAGS" \
+        -DCMAKE_CXX_COMPILER=$INSTALL_DIR/bin/clang++ \
+        -DCMAKE_CXX_FLAGS="$CXX_FLAGS" \
+        -DCMAKE_EXE_LINKER_FLAGS="$LINKER_FLAGS" \
+        -DCMAKE_SHARED_LINKER_FLAGS="$LINKER_FLAGS" \
+        -DLLVM_TARGETS_TO_BUILD=ARM \
+        -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_INCLUDE_EXAMPLES=OFF \
+        -DLLVM_EXTERNAL_LIBCXX_SOURCE_DIR=$LIBCXX_DIR \
+        -DLIBCXX_CXX_ABI=libstdc++ \
+        -DLIBCXX_CXX_ABI_LIBRARY_PATH=$LINARO/$TRIPLE/lib/libstdc++.a \
+        $LLVM_DIR
+}
+
+function build_libcxx()
+{
+    cd $LIBCXX_BUILD_DIR
+    make -j9 cxx
+}
+
+function install_libcxx()
+{
+    cd $LIBCXX_BUILD_DIR
+    make install-libcxx
 }
 
 for cmd in "$@"; do
