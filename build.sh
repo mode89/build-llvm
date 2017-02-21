614a7fc78e2bd3428c405970757816cf5d241e16
Mode <mode89@mail.ru>
2017-02-21 13:45:50 +0800

    rename directories

--- build.sh
+++ build.sh
@@ -3,17 +3,16 @@
 WORK_DIR=$(pwd)
 
 LLVM_REPO=http://llvm.org/git/llvm.git
-LLVM_DIR=.llvm
+LLVM_DIR=$WORK_DIR/llvm
 
 CLANG_REPO=http://llvm.org/git/clang.git
-CLANG_DIR=.clang
+CLANG_DIR=$WORK_DIR/clang
 
 function clone_llvm()
 {
     git clone $LLVM_REPO $LLVM_DIR
     cd $LLVM_DIR
     git checkout release_39
-    cd $WORK_DIR
 }
 
 function clone_clang()
@@ -21,7 +20,6 @@ function clone_clang()
     git clone $CLANG_REPO $CLANG_DIR
     cd $CLANG_DIR
     git checkout release_39
-    cd $WORK_DIR
 }
 
 $1
