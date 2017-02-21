a2d031505acbf63399737acb699bf73b6c3ba8b0
Mode <mode89@mail.ru>
2017-02-21 13:26:55 +0800

    clone clang repository

--- build.sh
+++ build.sh
@@ -5,10 +5,21 @@ WORK_DIR=$(pwd)
 LLVM_REPO=http://llvm.org/git/llvm.git
 LLVM_DIR=.llvm
 
+CLANG_REPO=http://llvm.org/git/clang.git
+CLANG_DIR=.clang
+
 function clone_llvm()
 {
     git clone $LLVM_REPO $LLVM_DIR
     cd $LLVM_DIR
+    git checkout release_39
+    cd $WORK_DIR
+}
+
+function clone_clang()
+{
+    git clone $CLANG_REPO $CLANG_DIR
+    cd $CLANG_DIR
     git checkout release_39
     cd $WORK_DIR
 }
